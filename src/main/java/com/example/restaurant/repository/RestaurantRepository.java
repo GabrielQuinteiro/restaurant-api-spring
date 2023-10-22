b3007eb2fbcea5e3d31ee8a481b61c3c217fc2c2
Gabriel <gabrieldecb@gmail.com>
2023-10-21 21:27:55 -0300

    feat: create restaurant repository

---
+++ src/main/java/com/example/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.example.restaurant.repository;
+
+import com.example.restaurant.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    boolean existsByName(String restaurantName);
+}
